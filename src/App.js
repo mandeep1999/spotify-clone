8a592c46d6ff06a91b2848b0821ed66e8fe63b6a
Mandeep Singh <mandeep.99@yahoo.com>
2020-08-28 08:51:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './pages/Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './pages/Player';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log('😁', user);
+      });
+    }
+    console.log('I have a Token', _token);
+  }, []);
+
+  return <div className="App">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
